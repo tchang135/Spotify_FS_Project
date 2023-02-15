9dd4a7e004f3072d4b0deced3c6f34a9aa69c4d4
Timothy Chang <tchang135@gmail.com>
2023-02-15 15:26:16 -0500

    fixed useSelectors and html code to now properly render all items on the show page

--- frontend/src/components/AlbumShow/index.js
+++ frontend/src/components/AlbumShow/index.js
@@ -8,22 +8,24 @@ import { useDispatch, useSelector } from 'react-redux';
 function AlbumShowPage() {
     const dispatch = useDispatch();
     const { albumId } = useParams();
-    const album = useSelector(state => state.albums[albumId])
+    const album = useSelector(state => {
+        return state.albums[albumId]
+    })
     const artist = useSelector(state => {
-        // state.artist[album.artistId])
-        const final = []
-        for (let i = 0; i < state.artist.length; i++) {
-          const currentArtist = state.artist[i]
+        const artistArr = Object.values(state.artist)
+        for (let i = 0; i < artistArr.length; i++) {
+          const currentArtist = artistArr[i]
           if (currentArtist.id === album.artistId) {
-            final.push(currentArtist)
+            return currentArtist
           }
         }
-        return final
+        return null
       })
     const songs = useSelector(state => {
-        const final = []
-        for (let i = 0; i < state.songs.length; i++) {
-          const currentSong = state.songs[i]
+        const final = [];
+        const songsArr = Object.values(state.songs)
+        for (let i = 0; i < songsArr.length; i++) {
+          const currentSong = songsArr[i]
           if (currentSong.albumId === album.id) {
             final.push(currentSong)
           }
@@ -46,19 +48,22 @@ function AlbumShowPage() {
             <div className='albumDescription'>
                 <div className='album'>
                     <p className='albumTitle'>{album.title}</p>
-                    <p className='publishedYear'>{album.publishedYear}</p>
+                    <p className='publishedYear'>{album.publishedDate}</p>
                     <p className='artistName'>{artist.name}</p> 
+                    <p class="artistDescription">{artist.description}</p>
                 </div>
 
             </div>
             <div className='albumSongs'>
                 <ul className='songList'>
                     {songs.map((song) => {
+                        return (
                     <li className="song" key={song.id}>    
                         <p id="songTitle"> {song.title} </p>
                         <p id="artistName"> {artist.name} </p>
                         <p id="songLength"> Length </p>
                     </li>  
+                        )
                     })}
                 </ul>
             </div> 
