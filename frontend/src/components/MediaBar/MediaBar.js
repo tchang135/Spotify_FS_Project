909b0d138debf66db5ed4547d1d375d7a024a478
Timothy Chang <tchang135@gmail.com>
2023-02-16 09:45:21 -0500

    working on media bar functionality

--- frontend/src/components/MediaBar/MediaBar.js
+++ frontend/src/components/MediaBar/MediaBar.js
@@ -1,14 +1,36 @@
-import AudioPlayer from 'react-h5-audio-player'
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { setCurrentSong } from '../../store/currentSong';
 
-const MediaBar = ({ trackUrl }) => {
+const MediaBar = ({ songUrl }) => {
+
+    const dispatch = useDispatch();
+    const currentSongId = useSelector((state) => state.currentSong)
+    const currentAlbumSongs = useSelector(({ currentAlbum }) => {
+        return currentAlbum.id ? Object.values(currentAlbum.songs) : []
+    });
 
 
+    const handleClickNext = () => {
+        const songId = currentSongId < currentAlbumSongs.length - 1 ? currentSongId + 1 : 0;
+        dispatch(setCurrentSong(songId));
+    };
+
+    const handleClickPrevious = () => {
+        const songId = currentSongId > 0 ? currentSongId - 1 : currentAlbumSongs.length - 1;
+        dispatch(setCurrentSong(songId));
+    };
 
 
 
     return (
         <AudioPlayer
-        
+        showSkipControls
+        onClickNext={handleClickNext}
+        onClickPrevious={handleClickPrevious}
+        onEnded={handleClickNext}
+        showFilledVolume
         
         />
     )
