59d24e0ff9b9c593de5dcc3363f9d7bb863b9765
Timothy Chang <tchang135@gmail.com>
2023-03-06 14:37:29 -0500

    handleclick made to click on search and lead to page

--- frontend/src/components/SearchPage/index.js
+++ frontend/src/components/SearchPage/index.js
@@ -3,11 +3,13 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { fetchAlbums } from "../../store/album";
 import { fetchSongs } from "../../store/song";
+import { useHistory } from "react-router-dom";
 import './SearchPage.css'
 
 
 const SearchPage = () => {
   const dispatch = useDispatch();
+  const history = useHistory();
   const [searchTerm, setSearchTerm] = useState("");
   const albums = useSelector(state => state.albums ? Object.values(state.albums) : []);
   const songs = useSelector(state => state.songs ? Object.values(state.songs) : []);
@@ -29,7 +31,9 @@ const SearchPage = () => {
       return null
   } 
 
-  
+  const handleClick = (albumId) => {
+    history.push(`/albums/${albumId}`);
+  };
 
   return (
     <div>
@@ -37,7 +41,7 @@ const SearchPage = () => {
       <ul className="searchResults">
         {filteredAlbum.map((item) => (
           <li key={item.id}>
-            <div className="resultItem">
+            <div className="resultItem" onClick={() => handleClick(item.id)} >
                 <img id="resultItemPhoto" src={item.photoUrl} alt=""/>
                 <p>{item.title}</p>
                 <p>{item.artistId}</p>
