4e7ba1f2e5b54f262ef8f187bb340f77911dd3b5
Timothy Chang <tchang135@gmail.com>
2023-03-08 14:32:17 -0500

    adding playlist controller

--- app/controllers/api/playlists_controller.rb
+++ app/controllers/api/playlists_controller.rb
@@ -1,2 +1,44 @@
 class Api::PlaylistsController < ApplicationController
+    def index
+        @playlists = Playlist.all 
+        render :index 
+    end
+
+    def show 
+        @playlist = Playlist.find(params[:id])
+        render :show
+    end
+
+    def create 
+        @playlist = Playlist.new(playlist_params)
+        @playlist.user_id = current_user.id
+
+        if @playlist.save 
+            render :show
+        else  
+            render json: @playlist.errors.full_messages, status: 422
+        end
+    end
+
+    def update 
+        @playlist = Playlist.find(params[:playlist_id])
+        
+        if @playlist.update(playlist_params)
+            render :show 
+        else  
+            render json: @playlist.errors.full_messages, status: 422
+        end
+    end
+
+    def destroy 
+        playlist = Playlist.find(params[:id])
+        if playlist 
+            playlist.destroy 
+            render :index 
+        else  
+            render json: ["Playlist doesn't exist"], status: 404
+        end
+    end
+
+    
 end
