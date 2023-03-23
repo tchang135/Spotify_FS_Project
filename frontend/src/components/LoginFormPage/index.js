0ffa2e46cea90cbee1974c381a9d43e5411a2355
Timothy Chang <tchang135@gmail.com>
2023-03-22 23:30:02 -0400

    some css changes on login/signup form and home page

--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import * as sessionActions from '../../store/session';
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect, Link } from 'react-router-dom';
-import './LoginForm.css';
 import logo from '../images/logo.JPG';
+import './LoginForm.css';
+
 
 function LoginFormPage() {
   const dispatch = useDispatch();
