e036aa65e2a57bf4f905bb10b9512cf93cb0c2bc
Timothy Chang <tchang135@gmail.com>
2023-03-22 22:47:08 -0400

    delete button deletes playlist

--- frontend/src/components/PlaylistShow/index.js
+++ frontend/src/components/PlaylistShow/index.js
@@ -1,19 +1,28 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchPlaylist } from "../../store/playlist";
+import { fetchPlaylist, deletePlaylist } from "../../store/playlist";
 import './PlaylistShow.css'
 
 const PlaylistShow = () => {
   const { playlistId } = useParams();
   const dispatch = useDispatch();
   const playlist = useSelector((state) => state.playlists);
+  const history = useHistory();
 
   useEffect(() => {
     if (playlistId) {
       dispatch(fetchPlaylist(playlistId));
     }
   }, [playlistId, dispatch]);
+
+  const handleDelete = () => {
+    if (window.confirm('Are you sure you want to delete this playlist?')) {
+      dispatch(deletePlaylist(playlistId)).then(() => {
+        history.push('/');
+      });
+    }
+  };
 
   if (!playlistId) {
     return <div>Loading...</div>;
@@ -23,6 +32,7 @@ const PlaylistShow = () => {
     <div className="newPlaylistPage">
       <h1 className="newPlaylistTitle">{playlist.title}</h1>
       <p className="newPlaylistDescription">{playlist.description}</p>
+      <button onClick={handleDelete}>Delete</button>
     </div>
   );
 };
--- frontend/src/store/playlist.js
+++ frontend/src/store/playlist.js
@@ -1,5 +1,6 @@
 export const RECEIVE_PLAYLIST = 'playlists/RECEIVE_PLAYLIST';
 export const RECEIVE_PLAYLISTS = 'playlists/RECEIVE_PLAYLISTS';
+export const REMOVE_PLAYLIST = 'playlists/REMOVE_PLAYLIST';
 
 export const receivePlaylists = (playlists) => ({
     type: RECEIVE_PLAYLISTS,
@@ -9,6 +10,11 @@ export const receivePlaylists = (playlists) => ({
 export const receivePlaylist = (playlist) => ({
     type: RECEIVE_PLAYLIST,
     playlist
+})
+
+export const removePlaylist = (playlistId) => ({
+  type: REMOVE_PLAYLIST,
+  playlistId
 })
 
 export const fetchPlaylists = () => async dispatch => {
@@ -23,6 +29,16 @@ export const fetchPlaylist = (playlistId) => async (dispatch) => {
     return dispatch(receivePlaylist(data));
 };
 
+export const deletePlaylist = (playlistId) => async (dispatch) => {
+  const response = await fetch(`/api/playlists/${playlistId}`, {
+    method: 'DELETE'
+  });
+
+  if (response.ok) {
+    dispatch(removePlaylist(playlistId));
+  }
+};
+
 const playlistsReducer = (state = {}, action) => {
     const nextState = { ...state };
 
@@ -31,6 +47,9 @@ const playlistsReducer = (state = {}, action) => {
             return { ...nextState, ...action.playlists };
         case RECEIVE_PLAYLIST:
             return { ...nextState, ...action.playlist };
+        case REMOVE_PLAYLIST:
+            delete nextState[action.playlistId];
+            return nextState;
         default:
             return state;
     }
